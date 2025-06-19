6af9d30fbd34b885f85d0ce9c8c810752b904018
javihen <62959381+javihen@users.noreply.github.com>
2025-06-18 20:00:37 -0400

    feat desestructuracion

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import './style.css'
 /* import './topics/01-basic-types'; */
 /* import './topics/02-objects-interface'; */
 /* import './topics/03-functions'; */
-import './topics/04-homework-types';
+/* import './topics/04-homework-types'; */
+/* import './topics/051-basic-destructuring'; */
+import './topics/06-function-destructuring';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
---
+++ src/topics/051-basic-destructuring.ts
@@ -0,0 +1,3 @@
+const [p1,p2,trunks='not found']: string[] = ['Goku', 'Vegeta', 'Trunks'];
+
+console.log('Personaje 3: ',trunks);
